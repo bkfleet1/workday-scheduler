55f7994e14ee243aaa3d6e65764941e633033ada
bkfleet1 <bkfleet1@gmail.com>
2021-12-26 11:11:44 -0600

    text area working, but not save retrieve yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,16 +64,16 @@ function hourItem(i) {
 }
 
 function formatHour(i) {
-    if (i == 8 || i == "8") { return "8AM" }
-    else if (i == 9 || i == "9") { return "9AM" }
-    else if (i == 10 || i == "10") { return "10AM" }
-    else if (i == 11 || i == "11") { return "11AM" }
-    else if (i == 12 || i == "12") { return "12PM" }
-    else if (i == 13 || i == "13") { return "1PM" }
-    else if (i == 14 || i == "14") { return "2PM" }
-    else if (i == 15 || i == "15") { return "3PM" }
-    else if (i == 16 || i == "16") { return "4PM" }
-    else if (i == 17 || i == "17") { return "5PM" }
+    if (i == 8 || i == "8") { return i + ":00 AM" }
+    else if (i == 9 || i == "9") { return i + ":00 AM" }
+    else if (i == 10 || i == "10") { return i + ":00 AM" }
+    else if (i == 11 || i == "11") { return i + ":00 AM" }
+    else if (i == 12 || i == "12") { return i + ":00 PM" }
+    else if (i == 13 || i == "13") { return i + ":00 PM" }
+    else if (i == 14 || i == "14") { return i + ":00 PM" }
+    else if (i == 15 || i == "15") { return i + ":00 PM" }
+    else if (i == 16 || i == "16") { return i + ":00 PM" }
+    else if (i == 17 || i == "17") { return i + ":00 PM" }
     else return i
 }
 
@@ -82,11 +82,11 @@ function textAreaItem(i, currentHour) {
     const timeSlotText = document.createElement("div");
     timeSlotText.classList.add(textAreaItemBG(i, currentHour), "col-md-10", "task-item");
     timeSlotText.id = "timeSlotText-" + i;
- //   timeSlotText.innerHTML = '<p></p>';
+ //   timeSlotText.innerHTML = '<textarea></textarea>';
     return timeSlotText;}
 
    function taskItem(i) {
-        const timeSlotTask = document.createElement("p");
+        const timeSlotTask = document.createElement("textarea");
         timeSlotTask.classList.add("task");
         timeSlotTask.id = "timeSlotTask-" + i;
         timeSlotTask.text = "";
@@ -113,42 +113,21 @@ function saveBtnItem(i) {
     return saveBtn;
 };
 
-    $(".task-item").on("click", function() {
-        var status = $(this)
-        .attr("id")
-         console.log(status);
+$(document).ready(function () {
+    $('.saveBtn').on('click',function () {
+        var savedTimeSlot = $(this).parent().attr('id');
+        var savedTask = $(this).siblings('task-item').val();
+        console.log(savedTimeSlot);
+        console.log(savedTask);
+        localStorage.setItem(savedTimeSlot, savedTask);
+    });
 
-        // get current text of p element
-        var text = $(this)
-          .text()
-          .trim();
-      
-        // replace p element with a new textarea
-        var textInput = $("<textarea>").addClass("form-control","task").val(text);
-        $(this).replaceWith(textInput);
-      
-        // auto focus new element
-        textInput.trigger("focus");
-     });
 
-    $(".task-item").on("blur", "textarea", function() {
-        // get current value of textarea
-        var text = $(this).val();
-      
-        // get status type and position in the list
-        var status = $(this)
-          .closest(".task")
-          .attr("id")
-          .replace(".task",".task");
-    
-       // update task in array and re-save to localstorage
-        tasks[status][index].text = text;
-      
-        // recreate p element
-        var taskP = $("<p>")
-          .addClass("m-1")
-          .text(text);
-      
-        // replace textarea with new content
-        $(this).replaceWith(taskP);
-      });
+    $('textarea').each(function () {
+        var storedTask = $(this).parent().attr('id');
+        var getStored = localStorage.getItem(storedTask);
+        if (getStored !== null) {
+            $(this).val(getStored);
+        }
+    });
+});
